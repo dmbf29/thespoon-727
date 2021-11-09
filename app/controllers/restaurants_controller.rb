929673330e8c7898913e1d2d3a520bd111a32340
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-09 11:04:56 +0900

    Created all 7 CRUD actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,56 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  # /restaurants
   def index
+    @restaurants = Restaurant.all
   end
 
+  # /restaurants/1
   def show
   end
 
+  # /restaurants/new
   def new
+    # create an empty instance so the form can build to the correct path/method
+    @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new.html.erb'
+    end
+  end
+
+  # '/restaurants/2/edit'
   def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'edit.html.erb'
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # strong params
+  # whitelist the params coming from the form
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # verb '/path', to: 'controller#action', as: :prefix
   # CRUD
-  # Read all
-  get '/restaurants', to: 'restaurants#index', as: :restaurants
-  # Read one # params[:id]
-  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
-
   # Create
   # one page for the form
   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
   # a place to send our form data
-  post '/restaurants', to: 'restaurants#create'
+  post '/restaurants', to: 'restaurants#create', as: :restaurants
+
+  # Read all
+  get '/restaurants', to: 'restaurants#index'
+  # Read one # params[:id]
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
 
   # Update
   # one page for the form
