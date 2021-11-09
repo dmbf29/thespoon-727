27a1fb7ebda72c25dcaaf51a97b688577745a6a5
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-09 10:00:07 +0900

    added routes and resturants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb '/path', to: 'controller#action', as: :prefix
+  # CRUD
+  # Read all
+  get '/restaurants', to: 'restaurants#index', as: :restaurants
+  # Read one # params[:id]
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Create
+  # one page for the form
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # a place to send our form data
+  post '/restaurants', to: 'restaurants#create'
+
+  # Update
+  # one page for the form
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # a place to send our form data
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Destroy
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 end
+
+
+# index
+# restaurants_path, method: :get
+# create
+# restaurants_path, method: :post
+# show
+# restaurant_path(restaurant), method: :get
+# update
+# restaurant_path(restaurant), method: :patch
+# destroy
+# restaurant_path(restaurant), method: :delete
